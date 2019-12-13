921ded3dadad319c08fc5e6da293d1ef4b9a2cbb
Jan Wolff <janw@mailbox.org>
2019-12-13 23:38:08 +0100

    back up and running bitches

---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    public Transform target;
+    public float damping;
+    public Vector3 mod;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(!target) return;
+
+        Vector3 targetPos = target.position + mod;
+        transform.position = Vector3.Lerp(transform.position, targetPos, damping * Time.deltaTime);
+    }
+}
